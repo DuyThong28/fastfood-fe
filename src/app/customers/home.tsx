526f4e35a44b8096279ad83de9243a5a1f12afd6
duy thong <duythong28112004@gmail.com>
2024-11-06 03:59:58 +0700

    feat: fech all books

---
+++ src/app/customers/home.tsx
@@ -0,0 +1,47 @@
+import { BookStatus } from "@/common/enums";
+import ProductLayout from "@/components/layouts/product-layout";
+import ProductItemCard from "@/components/product/product-item-card";
+import bookService from "@/services/book.service";
+import { Meta } from "@/types/api";
+import { ResBookDetail } from "@/types/book";
+import {  useEffect, useState } from "react";
+
+export default function HomeRoute() {
+  const [books, setBooks] = useState<ResBookDetail[]>([]);
+  const [meta, setMeta] = useState<Meta>({
+    page: 1,
+    take: 20,
+    itemCount: 0,
+    pageCount: 0,
+    hasPreviousPage: false,
+    hasNextPage: false,
+  });
+  const getAllBooks = async () => {
+    try {
+      const response = await bookService.getAllBooks({
+        page: meta.page,
+        take: meta.take,
+      }, BookStatus.ACTIVE);
+
+      setBooks(response.data.data);
+      setMeta(response.data.meta);
+   
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getAllBooks();
+  }, [meta.page]);
+
+  return (
+    <ProductLayout>
+      <div className="w-full grid grid-cols-5 gap-4 py-4">
+        {books.map((item, index) => {
+          return <ProductItemCard key={index} data={item} />;
+        })}
+      </div>
+    </ProductLayout>
+  );
+}
---
+++ src/components/layouts/product-layout.tsx
@@ -0,0 +1,42 @@
+import { Button } from "@/components/ui/button";
+import {  Search, ShoppingCart } from "lucide-react";
+import UserDropDownMenu from "../shared/user-drop-down-menu";
+import { useNavigate } from "react-router-dom";
+import { routes } from "@/config";
+
+export default function ProductLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const navigation = useNavigate();
+  return (
+    <div className="h-screen w-full grid grid-rows-[96px_1fr]">
+      <div className="px-40 h-full w-full flex flex-row items-center bg-black gap-16">
+        <a href="/" className="text-white text-nowrap  text-2xl font-bold">
+          Book Now
+        </a>
+
+        <div className="flex  w-full rounded-sm items-center space-x-2 bg-white p-[2px]">
+          <input
+            className="w-full pl-2"
+            type="text"
+            placeholder="Tim sach..."
+          />
+          <Button className="rounded-sm" type="submit">
+            <Search className="w-6 h-6 text-white" />
+          </Button>
+        </div>
+        <div className="flex flex-row gap-4 w-fit items-center">
+          <div className="w-11" onClick={()=> navigation(routes.CUSTOMER.CART)}>
+            <ShoppingCart className="h-7 w-7 text-white" />
+          </div>
+          <div className="w-11">
+            <UserDropDownMenu />
+          </div>
+        </div>
+      </div>
+      <div className="px-40 flex flex-col gap-6 bg-[#f9f9f9] overflow-y-auto">{children}</div>
+    </div>
+  );
+}
