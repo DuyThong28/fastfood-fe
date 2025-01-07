ef8176a9e37792c60789b914e4fc7e4ee994df1f
quark1412 <hvphuongdts@gmail.com>
2025-01-07 21:21:09 +0700

    refactor: fetch data when change period or date

--- src/app/routes/admin/report/product-tab.tsx
+++ src/app/routes/admin/report/product-tab.tsx
@@ -104,10 +104,10 @@ export default function ProductTab() {
 
   const handleViewChange = (value: string) => {
     setSelectedView(value);
-    const currentDate = dayjs();
-    setInputValue(formatInputValue(currentDate));
-    setSelectedDate(currentDate);
-    fetchData();
+    // const currentDate = dayjs();
+    // setInputValue(formatInputValue(currentDate));
+    // setSelectedDate(currentDate);
+    // fetchData();
   };
 
   const fetchData = async () => {
@@ -196,11 +196,10 @@ export default function ProductTab() {
   }, []);
 
   useEffect(() => {
-    const currentDate = dayjs();
-    setSelectedDate(currentDate);
-    setInputValue(formatInputValue(currentDate));
-    fetchData();
-  }, [selectedView]);
+    if (selectedDate) {
+      setInputValue(formatInputValue(selectedDate));
+    }
+  }, [selectedDate, selectedView]);
 
   useEffect(() => {
     fetchData();
